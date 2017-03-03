0681acdbcd14bcefe213b64e1e1e4c97b34fbb85
GELE Axel <ga605434@s202p09m.iutnice.unice.fr>
2017-03-03 13:12:37 +0100

    Initial Commit

---
+++ Mario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mario
+//
+//  Created by GELE Axel on 03/03/2017.
+//  Copyright © 2017 GELE Axel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
